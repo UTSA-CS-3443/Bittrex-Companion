3507cc911e70d9de6b78a226ee683ebbc58bf894
pss904 <pss904@my.utsa.edu>
2017-11-09 16:48:14 -0600

    Added the ability to gather data for algorithm testing.

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,16 +1,23 @@
 package application;
 import apis.Coin;
+import java.io.PrintWriter;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+
+import java.io.File;
 import java.util.Date;
 
 
 public class Main extends Application {
 	//set this to true to run the below code in main
 	public static final boolean TEST = false;
+	
+	//set this to true and set the int value to the number of seconds to wait between pulls 
+	public static final boolean GATHER_DATA = true;
+	public static final long TIME_TO_WAIT_IN_SECONDS = 10;
 	
 	@Override
 	public void start(Stage primaryStage) {
@@ -43,15 +50,33 @@ public class Main extends Application {
 				System.out.println("\tPrevious Day: " + totalVals[i][0]);
 			}
 		}
-		Coin testCoin = new Coin("LTC", "", "");
-		String hello;
-		while (true) {
-			Thread.sleep(10000);
-			testCoin = new Coin("LTC", "", "");
-			hello = testCoin.getMarketSummary("TimeStamp");
-			System.out.println(hello);
-
+		
+		if (GATHER_DATA) {
+			String[] coins = {"ETH", "NEO", "BCC", "VTC", "ADA", "OMG", "XRP", "LTC", "LSK", "GRS"};
+			String temp;
+			String[] split = new String[2];
+			String[][] coinVals = new String[10][2];
+			File outFile = new File("TestData.txt");
+			PrintWriter out = new PrintWriter(outFile);
+			if (!outFile.exists()) {
+				outFile.createNewFile();
+			}
+			while (true) {
+				for (int i = 0; i < 10; i++) {
+					Coin tempCoin = new Coin(coins[i], "", "");
+					temp = tempCoin.getMarketSummary("TimeStamp");
+					split = temp.split("T");
+					split[1] = split[1].replace('"', ' ').trim();
+					split[0] = tempCoin.getTicker("Bid");
+					coinVals[i][0] = split[1];
+					coinVals[i][1] = split[0];
+					out.println(coins[i] + "," + coinVals[i][0] + "," + coinVals[i][1]);
+					out.flush();
+				}
+				Thread.sleep(TIME_TO_WAIT_IN_SECONDS * 1000);
+			}
 		}
+		
 		
 		
 		//launch(args);
