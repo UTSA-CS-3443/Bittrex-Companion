962d3553b711639bd3f11a3bc087cfa4920e590b
pss904 <pss904@my.utsa.edu>
2017-11-25 15:11:02 -0600

    Fixed Analyzer class and removed all tests from Main

--- src/application/Analyzer.java
+++ src/application/Analyzer.java
@@ -1,5 +1,6 @@
 package application;
 import java.util.ArrayList;
+import apis.Coin;
 
 /**
  * Class designed to handle the analysis of all the data and 
@@ -14,108 +15,47 @@ import java.util.ArrayList;
  */
 
 public class Analyzer {
-	private ArrayList<Double> rangeReturn;
-	private double[] data;
-	private double[] oldNew;
-	private int[] indexRange;
-	private int dataLength;
-	private double percentThreshold;
-	private boolean checkForThresh;
 	
-	/**
-	 * Constructor for class. 
-	 * 
-	 * @param data Array of doubles containing the data you wish to analyze
-	 * @param percentThreshold The threshold you wish to check against
-	 */
-	public Analyzer(double[] data, double percentThreshold) {
-		this.data = data;
-		this.oldNew = new double[2];
-		this.dataLength = this.data.length;
-		this.percentThreshold = percentThreshold;
-		this.rangeReturn = new ArrayList<Double>();
-		this.indexRange = new int[2];
-	}
+	private Coin[] coinsList;
+	private ArrayList<Coin> coinsToReturn;
+	private boolean success;
+	private double volume, low, high, pChange, current;
 	
-	/**
-	 * The actual analysis method. Will check the array for a percent increase
-	 * or decrease that matches the supplied parameters.
-	 * @return boolean True if a match is found, false otherwise
-	 */
-	public boolean analyze() {
-		int i;
-		double tempPercentChange;
-		for (i = 0; i < this.dataLength; i++) {
-			if (data[i+1] > data[i]) {
-				i = increasing(i);
-			} else if (data[i+1] < data[i]){
-				i = decreasing(i);
-			}
-			tempPercentChange = percentChange(oldNew[0], oldNew[1]);
-			if (tempPercentChange > percentThreshold || tempPercentChange < percentThreshold) {
-				checkForThresh = true;
-				return true;
-			}
-		}
-		checkForThresh = false;
-		return false;
+	public Analyzer(Coin[] coinsList) {
+		this.coinsList = coinsList;
+		this.coinsToReturn = new ArrayList<Coin>();
+		success = false;
+		analyze();
 		
 	}
-	/**
-	 * Method to return an ArrayList of the values in the range that meets the
-	 * threshold. May be deprecated
-	 * @return ArrayList<Double> The values that met the criteria
-	 */
-	public ArrayList<Double> getValueArray() {
-		if (checkForThresh) {
-			for (int i = indexRange[0]; i < indexRange[1]; i++) {
-				this.rangeReturn.add(this.data[i]);
-			}
-			return this.rangeReturn;
-		} 
-		return null;
-	}
-	/**
-	 * Find the range of increasing values
-	 * @param startIndex Local minimum to begin the range
-	 * @return int The ending index so the process can be repeated
-	 */
-	private int increasing(int startIndex) {
-		int i = startIndex;
-		while (data[i+1] > data[i]) {
-			i++;
+	
+	private void analyze() {
+		for (Coin temp : this.coinsList) {
+			this.volume = Double.valueOf(temp.getMarketSummary("BaseVolume"));
+			this.high = Double.valueOf(temp.getMarketSummary("High"));
+			this.low = Double.valueOf(temp.getMarketSummary("Low"));
+			this.current = Double.valueOf(temp.getMarketSummary("Last"));
+			this.pChange = percentChange(this.low, this.high);
+			if (this.pChange >= 5 && this.volume >= 300)
+				coinsToReturn.add(temp);
 		}
-		oldNew[0] = this.data[startIndex];
-		oldNew[1] = this.data[i];
-		indexRange[0] = startIndex;
-		indexRange[1] = i;
-		return i;
+		if (coinsToReturn.size() >= 5)
+			this.success = true;
 	}
-	/**
-	 * Find the range of decreasing values
-	 * @param startIndex Local maximum to begin the range
-	 * @return int The ending index so the process can be repeated
-	 */
-	private int decreasing(int startIndex) {
-		int i = startIndex;
-		while (data[i+1] < data[i]) {
-			i++;
-		}
-		oldNew[0] = this.data[startIndex];
-		oldNew[1] = this.data[i];
-		indexRange[0] = startIndex;
-		indexRange[1] = i;
-		return i;
+	
+	public double priorityScore() {
+		return percentChange(this.low, this.current);
 	}
-	/**
-	 * Method to calculate the percent change of a range of values
-	 * Formula: (N - O) / O * 100
-	 * @param oldVal The old value for the formula
-	 * @param newVal The new value for the formula
-	 * @return double The value of the percent change
-	 */
+	
 	private double percentChange(double oldVal, double newVal) {
 		return ((newVal - oldVal) / oldVal) * 100;
 	}
 	
+	public boolean checkForSuccess() {
+		return this.success;
+	}
+	
+	public ArrayList<Coin> getCoins() {
+		return this.coinsToReturn;
+	}
 }
--- src/application/Main.java
+++ src/application/Main.java
@@ -14,15 +14,6 @@ import javafx.scene.web.WebView;
 import java.io.File;
 
 public class Main extends Application {
-	//set this to true to run the below code in main
-	public static final boolean TEST = false;
-	
-	//set this to true and set the int value to the number of seconds to wait between pulls 
-	public static final boolean GATHER_DATA = false;
-	public static final long TIME_TO_WAIT_IN_SECONDS = 10;
-	
-	//set this to true to test the analyzer
-	public static final boolean TEST_ALGORITHM = true;
 	
 	@Override
 	public void start(Stage primaryStage) {
@@ -38,70 +29,6 @@ public class Main extends Application {
 	}
 	
 	public static void main(String[] args) throws Exception {
-		//If you want this to run, set TEST above to true
-		if (TEST) {
-			String[] coins = {"ETH", "NEO", "VTC", "BCC", "OMG"};
-			String[][] totalVals = new String[5][4];
-			for (int i = 0; i < 5; i++) {
-				Coin newCoin = new Coin(coins[i], "", "");
-				totalVals[i][0] = newCoin.getMarketSummary("High");
-				totalVals[i][1] = newCoin.getMarketSummary("Low");
-				totalVals[i][2] = newCoin.getMarketSummary("Volume");
-				totalVals[i][3] = newCoin.getMarketSummary("PrevDay");
-				System.out.println("Coin: " + coins[i]);
-				System.out.println("\tHigh: " + totalVals[i][0]);
-				System.out.println("\tLow: " + totalVals[i][1]);
-				System.out.println("\tVolume: " + totalVals[i][0]);
-				System.out.println("\tPrevious Day: " + totalVals[i][0]);
-			}
-		}
-		
-		if (GATHER_DATA) {
-			String[] coins = {"ETH", "NEO", "BCC", "VTC", "ADA", "OMG", "XRP", "LTC", "LSK", "GRS"};
-			String temp;
-			String[] split = new String[2];
-			String[][] coinVals = new String[10][2];
-			File outFile = new File("TestData.txt");
-			PrintWriter out = new PrintWriter(outFile);
-			if (!outFile.exists()) {
-				outFile.createNewFile();
-			}
-			while (true) {
-				for (int i = 0; i < 10; i++) {
-					Coin tempCoin = new Coin(coins[i], "", "");
-					temp = tempCoin.getMarketSummary("TimeStamp");
-					split = temp.split("T");
-					split[1] = split[1].replace('"', ' ').trim();
-					split[0] = tempCoin.getTicker("Bid");
-					
-					
-					coinVals[i][0] = split[1];
-					coinVals[i][1] = split[0];
-					//print COIN NAME, TIMESTAMP, BID
-					out.println(coins[i] + "," + coinVals[i][0] + "," + coinVals[i][1]);
-				}
-				out.println();
-				out.flush();
-				Thread.sleep(TIME_TO_WAIT_IN_SECONDS * 1000);
-			}
-		}
-		
-		if (TEST_ALGORITHM) {
-			String[] coins = {"ETH", "NEO", "BCC", "VTC", "ADA", "OMG", "XRP", "LTC", "LSK", "GRS"};
-			Analyzer ana = new Analyzer(coins);
-			ArrayList<String> coinsList = new ArrayList<String>();
-			if (ana.checkForSuccess()) {
-				coinsList = ana.getCoins();
-				System.out.println(coinsList.size());
-			} else {
-				System.out.println("Failure");
-			}
-			
-			
-		}
-		
-		
-		
-		//launch(args);
+		launch(args);
 	}
 }
